f58de95e7d3014c31bfdeb281e1123e3b437875f
Rosa Chabane <rmiliziano@yahoo.com>
2023-09-11 20:06:20 -0500

    fixed issue that would not allow employee creation if all employees were deleted due to how employeeElements clone. Added functionality to edit schedule notes/date/shifts and touched up html/css of hours calculator

--- js/main.js
+++ js/main.js
@@ -27,23 +27,12 @@ class EmployeeManager {
       this.employeeSlotsHTMLArray.push(employeeElement.outerHTML);
 
       this.saveEmployeeData();
+      location.reload();
 
       employeeElement.addEventListener('dragstart', (e) => {
         e.dataTransfer.setData('text/plain', e.target.id);
       });
     }
-    employeeElement.addEventListener('click', () => {
-      const choice = confirm("Delete employee?");
-      if (choice) {
-        const index = this.employees.findIndex(emp => emp.id === employeeElement.id);
-        if (index !== -1) {
-          this.employees.splice(index, 1);
-          this.employeeSlotsHTMLArray.splice(index, 1);
-          this.saveEmployeeData();
-          employeeElement.remove();
-        }
-      }
-    });
   }
 
   addEmployee() {
@@ -73,15 +62,55 @@ class EmployeeManager {
     const employeesJSON = localStorage.getItem('employeesData');
     const loadedEmployees = JSON.parse(employeesJSON);
 
+    const dateText = localStorage.getItem('dateText');
+    document.getElementById('dateOfWeek').innerHTML = dateText;
+
+    const infoText = localStorage.getItem('infoText');
+    document.getElementById('extraInfo').innerHTML = infoText;
+
+    const amShiftHP1 = localStorage.getItem('amShiftHP1');
+    document.getElementById('amShiftHP1').innerHTML = amShiftHP1;
+
+    const pmShiftHP1 = localStorage.getItem('pmShiftHP1');
+    document.getElementById('pmShiftHP1').innerHTML = pmShiftHP1;
+
+    const amShiftHP2 = localStorage.getItem('amShiftHP2');
+    document.getElementById('amShiftHP2').innerHTML = amShiftHP2;
+
+    const pmShiftHP2 = localStorage.getItem('pmShiftHP2');
+    document.getElementById('pmShiftHP2').innerHTML = pmShiftHP2;
+
+    if (!dateText) {
+      document.getElementById('dateOfWeek').innerHTML = "click to edit"
+    }
+
+    if (!infoText) {
+      document.getElementById('extraInfo').innerHTML = "click to edit";
+    }
+    
+    if (!amShiftHP1) {
+      document.getElementById('amShiftHP1').innerHTML = "click to edit"
+    }
+
+    if (!pmShiftHP1) {
+      document.getElementById('pmShiftHP1').innerHTML = "click to edit"
+    }
+
+    if (!amShiftHP2) {
+      document.getElementById('amShiftHP2').innerHTML = "click to edit"
+    }
+
+    if (!pmShiftHP2) {
+      document.getElementById('pmShiftHP2').innerHTML = "click to edit"
+    }
+
     if (loadedEmployees) {
       this.employees = loadedEmployees.map(loadedEmployee => {
         return new Employee(loadedEmployee.id, loadedEmployee.name);
       });
 
       const employeeSlotsHTMLArray = JSON.parse(localStorage.getItem('employeeSlotsHTML'));
-      console.log(employeeSlotsHTMLArray);
       const employeeSlotsHTML = employeeSlotsHTMLArray.join('');
-      console.log(employeeSlotsHTML);
       document.querySelector('.employeeBlockContainer').innerHTML = employeeSlotsHTML;
 
       this.employees.forEach(employee => {
@@ -94,13 +123,19 @@ class EmployeeManager {
 
         employeeElement.addEventListener('click', () => {
           const choice = confirm("Delete employee?");
-          if (choice) {
+
+          if (choice && this.employees.length < 2) {
+            confirm("CANNOT REMOVE ONLY REMAINING EMPLOYEE");
+          }
+
+          if (choice && this.employees.length >= 2) {
             const index = this.employees.findIndex(emp => emp.id === employee.id)
             if (index !== -1) {
               this.employees.splice(index, 1);
               this.employeeSlotsHTMLArray.splice(index, 1);
               this.saveEmployeeData();
               employeeElement.remove();
+              location.reload();
             }
           }
         });
@@ -122,6 +157,7 @@ window.addEventListener('load', () => {
   employeeManager.loadEmployeeData();
 });
 
+// WeekdaySlot event listeners
 weekDaysSlots.forEach(slot => {
   slot.addEventListener('dragover', (e) => {
     e.preventDefault();
@@ -159,13 +195,48 @@ weekDaysSlots.forEach(slot => {
   });
 });
 
-//CAN STORE THE ENTIRE WEEK/CALENADR CONTAINER INTO LOCAL STORAGE TO SAVE DROPPED EMPLOYEES
+// Date/Note/shifts event listeners
 
-// FIX BUG: IF EMPLOYEE PROMPT IS EMPTY, DO NOT ADD EMPLOYEE if employee === null??
+const dateOfWeek = document.getElementById('dateOfWeek');
+const extraInfo = document.getElementById('extraInfo');
+const amShiftHP1 = document.getElementById('amShiftHP1');
+const pmShiftHP1 = document.getElementById('pmShiftHP1');
+const amShiftHP2 = document.getElementById('amShiftHP2');
+const pmShiftHP2 = document.getElementById('pmShiftHP2');
 
-//NEED TO UPDATE:
+dateOfWeek.addEventListener('click', () => {
+  const date = prompt("Enter text");
+  dateOfWeek.innerText = date;
+  localStorage.setItem('dateText', dateOfWeek.innerHTML);
+});
 
-// DO NOT LET DRAGGABLE ELEMENTS DRAG PAST OUTSIDE OF CALENDAR/EMPLOYEE CONTAINER 
+extraInfo.addEventListener('click', () => {
+  const info = prompt("Enter text");
+  extraInfo.innerText = info;
+  localStorage.setItem('infoText', extraInfo.innerHTML);
+});
 
-// SAVED DROPPED EMPLOYEE ELEMENTS INSIDE OF WEEKDAYSLOTS INTO LOCAL STORAGE
+amShiftHP1.addEventListener('click', () => {
+  const shift = prompt("Enter shift info");
+  amShiftHP1.innerText = shift;
+  localStorage.setItem('amShiftHP1', amShiftHP1.innerHTML);
+});
+
+pmShiftHP1.addEventListener('click', () => {
+  const shift = prompt("Enter shift info");
+  pmShiftHP1.innerText = shift;
+  localStorage.setItem('pmShiftHP1', pmShiftHP1.innerHTML);
+});
+
+amShiftHP2.addEventListener('click', () => {
+  const shift = prompt("Enter shift info");
+  amShiftHP2.innerText = shift;
+  localStorage.setItem('amShiftHP2', amShiftHP2.innerHTML);
+});
+
+pmShiftHP2.addEventListener('click', () => {
+  const shift = prompt("Enter shift info");
+  pmShiftHP2.innerText = shift;
+  localStorage.setItem('pmShiftHP2', pmShiftHP2.innerHTML);
+});
 
